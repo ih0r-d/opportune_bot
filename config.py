1bc86c0f5f75c219df591c42e37455775cf03ff0
Ihor Didyk <ihordidyk95@gmail.com>
2019-08-22 21:48:15 +0300

    addecd config for project

---
+++ config.py
@@ -0,0 +1,21 @@
+from lang import ua
+
+token = ''  # Set here your key
+
+log_name = 'opportune_bot'  # Log file name
+log_level = 'debug'  # debug, info, warning, error
+
+# Server offset from GMT (f.ex Ukraine is 2)
+server_offset = 0
+
+database_offsets_file = 'users_offsets'
+database_states_file = 'state_storage'
+database_temp_time_storage = 'temp_time_storage'
+database_schedules_file = 'schedules.db'
+
+# Language of bot. Look into "lang" folder for more
+lang = ua
+
+# Allows to temporally discard all messages except yours
+# Looks at app.py (cmd_closed_mode function)
+is_closed = False
